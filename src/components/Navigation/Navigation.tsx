0900c7526e34570d0916c21583b6ef485ad4de6d
kasi <danielkaser@mailbox.org>
2024-02-01 18:33:17 +0100

    create Navigation

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
+
+export default function Navigation() {
+  return (
+    <nav className="container flex items-center justify-between">
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+      <ThemeSwitch />
+    </nav>
+  );
+}
