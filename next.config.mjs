c3b106e8c52463939aca3862cf12e19dab39961b
kasi <danielkaser@mailbox.org>
2024-02-02 14:33:15 +0100

    add allowed img sources to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.twelvedata.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "logo.twelvedata.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
