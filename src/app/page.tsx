3855426c8cd2f668eb9c0d99e15af41bf48adf0d
kasi <danielkaser@mailbox.org>
2024-02-05 19:19:31 +0100

    set up data fetch from dev mock api in Home and clean up mock data from MongoDB export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
 import { StocksTable } from "@/components";
+import { Stock } from "../../types/types";
 
-export default function Home() {
+console.log("render Home");
+
+// async function getStocks(): Promise<Stock[]> {
+async function getStocks(): Promise<any[]> {
+  const data = await fetch("http://localhost:4000/stocks");
+  return data.json();
+}
+
+// clean demo data export from MongoDB
+function cleanStocks(stocks: any[]) {
+  return stocks.map((stock) => {
+    return {
+      ...stock,
+      _id: stock._id.$oid,
+      dividendDate: stock.dividendDate.$date,
+      ebitda: stock.ebitda?.$numberLong,
+      exDividendDate: stock.exDividendDate.$date,
+      grossProfitTTM: stock.grossProfitTTM.$numberLong,
+      latestQuarter: stock.latestQuarter.$date,
+      marketCapitalization: stock.marketCapitalization.$numberLong,
+      revenueTTM: stock.revenueTTM.$numberLong,
+      sharesOutstanding: stock.sharesOutstanding.$numberLong,
+      updatedAt: stock.updatedAt.$date,
+    };
+  });
+}
+
+export default async function Home() {
+  const stocks: any[] = await getStocks();
+  const cleanedStocks: Stock[] = cleanStocks(stocks);
+
   return (
     <section>
-      <StocksTable />
+      <StocksTable stocks={cleanedStocks} />
+      {/* <StocksTable stocks={stocks} /> */}
     </section>
   );
 }
