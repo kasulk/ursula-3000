3675836432693145339e7559a31a8701a0eaa197
kasi <danielkaser@mailbox.org>
2024-02-09 16:22:48 +0100

    change NextThemeProvider import to dynamic to set ssr to false

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,20 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemeProvider } from "next-themes";
+// import { ThemeProvider as NextThemeProvider } from "next-themes";
+//:: WORKAROUND:
+//:: Fixes warnings in browser console:
+//:: 1. "Prop `data-key` did not match.""
+//:: 2. "Extra attributes from the server: class,style"
+//:: https://github.com/nextui-org/nextui/issues/1729#issuecomment-1889921451
+//:: vvvvv
+const NextThemeProvider = dynamic(
+  () => import("next-themes").then((mod) => mod.ThemeProvider),
+  { ssr: false },
+);
+//:: ^^^^^
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
