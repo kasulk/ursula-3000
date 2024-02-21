ff1ac0405d06767edd1991e1bef4322a84fa7efa
kasi <danielkaser@mailbox.org>
2024-02-21 16:15:58 +0100

    add Button linked to AllStocks to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { GlowText } from "@/components";
+import { Button } from "@nextui-org/react";
 
 console.log("render Home");
 
@@ -9,11 +11,19 @@ export default function Home() {
         <p className="text-5xl font-bold">
           Meet <GlowText variant="orange pink">Ursula</GlowText>!
         </p>
-        <h3 className="text-3xl font-bold">
+        <h3 className="mb-12 text-3xl font-bold">
           Find potentially{" "}
           <GlowText variant="secondary primary">cheap stocks</GlowText> fast and
           easy.
         </h3>
+        <Link href="/AllStocks">
+          <Button
+            radius="md"
+            className="bg-gradient-to-tr from-pink-500 to-orange-500 text-white shadow-lg"
+          >
+            Unbelieveable!
+          </Button>
+        </Link>
       </div>
     </section>
   );
