72db50dce5f7171133496d0fa9a46fe9a77a06d9
kasi <danielkaser@mailbox.org>
2024-02-21 15:56:02 +0100

    implement GlowText into landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { GlowText } from "@/components";
+
 console.log("render Home");
 
 export default function Home() {
   return (
-    <section>
-      <h1>Welcome!</h1>
+    <section id="welcome">
+      <div className="flex flex-col items-center justify-center gap-12">
+        <p className="text-5xl font-bold">
+          Meet <GlowText variant="orange pink">Ursula</GlowText>!
+        </p>
+        <h3 className="text-3xl font-bold">
+          Find potentially{" "}
+          <GlowText variant="secondary primary">cheap stocks</GlowText> fast and
+          easy.
+        </h3>
+      </div>
     </section>
   );
 }
