001bbdeaeadf8652fb88d1b4fa497a8b663531df
kasi <danielkaser@mailbox.org>
2024-02-21 15:55:07 +0100

    create GlowText component with gradient

---
+++ src/components/GlowText/GlowText.tsx
@@ -0,0 +1,32 @@
+type Variant =
+  | "orange red"
+  | "orange pink"
+  | "primary secondary"
+  | "secondary primary";
+
+interface GlowTextProps {
+  variant: Variant;
+  children: string;
+}
+
+export function GlowText({ variant, children }: GlowTextProps) {
+  const variants: { [key: string]: string } = {
+    "orange red": "bg-gradient-to-r from-orange-500 to-red-500",
+    "orange pink": "bg-gradient-to-r from-orange-500 to-pink-500",
+    "primary secondary": "bg-gradient-to-r from-primary-500 to-secondary-500",
+    "secondary primary": "bg-gradient-to-r from-secondary-500 to-primary-500",
+  };
+
+  return (
+    <span className="relative">
+      <span className={`${variants[variant]} bg-clip-text text-transparent`}>
+        {children}
+      </span>
+      <span
+        className={`absolute left-0 blur-2xl ${variants[variant]} bg-clip-text text-transparent`}
+      >
+        {children}
+      </span>
+    </span>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 export * from "./NavBar/NavBar";
 export * from "./Logo/Logo";
 export * from "./ThemeSwitch/ThemeSwitch";
+export * from "./GlowText/GlowText";
 
 export * from "./StocksTable/StocksTable";
 export * from "./StocksTable/OriginalTable"; //:: unaltered table from https://nextui.org/docs/components/table#use-case-example
