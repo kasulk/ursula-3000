a3b401754de1a14e6f1fc73e64941bd8e28b3d58
kasi <danielkaser@mailbox.org>
2024-03-01 14:49:41 +0100

    create unique random username on login with provider

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -8,7 +8,7 @@ const userSchema = new Schema(
       type: String,
       unique: true,
       min: 3,
-      max: 20,
+      max: 30,
     },
     password: {
       type: String,
@@ -23,14 +23,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      max: 50,
+      max: 60,
     },
     avatar: {
       type: String,
     },
     role: {
       type: String,
-      enum: ["admin", "user", "googleUser", "githubUser"],
+      // enum: ["admin", "user", "googleUser", "githubUser"],
     },
     favoriteStocks: {
       type: [String],
