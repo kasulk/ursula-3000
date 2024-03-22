9b7117de7340065d8bee648c80504932c85f63db
kasi <danielkaser@mailbox.org>
2024-03-22 13:26:29 +0100

    add createLike() to /queries/likes.ts

--- src/db/models/Like.ts
+++ src/db/models/Like.ts
@@ -8,8 +8,8 @@ const likeSchema = new mongoose.Schema(
     },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      required: true,
       ref: "User",
+      required: true,
     },
   },
   { timestamps: true },
--- src/db/queries/likes.ts
+++ src/db/queries/likes.ts
@@ -1,12 +1,30 @@
+import mongoose from "mongoose";
 import dbConnect from "../connect";
 import { Like } from "../models";
 
 type Ticker = string;
 
-export async function getLikedStocksByUser(userId: any): Promise<Ticker[]> {
-  dbConnect();
+export async function getLikedStocksByUser(userId: string): Promise<Ticker[]> {
+  await dbConnect();
   const userLikes = await Like.find({ userId }).lean();
   const likedTickers = userLikes.map((like) => like.ticker); // ['AMD', 'MCD' ...]
 
   return likedTickers;
 }
+
+export async function createLike(
+  userIdString: string,
+  ticker: string,
+): Promise<void> {
+  await dbConnect();
+  const like = new Like({
+    userId: new mongoose.Types.ObjectId(userIdString),
+    ticker,
+  });
+
+  await like.save();
+}
+
+// const userIdString = "507f1f77bcf86cd799439011";
+// const ticker = "AAPL";
+// createLike(userIdString, ticker).then(() => console.log("Like saved"));
