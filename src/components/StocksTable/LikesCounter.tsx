334c5f2917b56fa8dc5b2d4f55ab1ef227c39206
kasi <danielkaser@mailbox.org>
2024-03-28 10:29:49 +0100

    only show likes count if there are any likes

--- src/components/StocksTable/LikesCounter.tsx
+++ src/components/StocksTable/LikesCounter.tsx
@@ -3,6 +3,8 @@ import { useLikedStocksStore } from "@/store/likedStocks";
 export default function LikesCounter() {
   const likedTickers = useLikedStocksStore((state) => state.tickers);
 
+  if (!likedTickers.length) return;
+
   return (
     <span className="absolute -top-1 left-10 z-10 flex items-center justify-center rounded-full bg-danger px-2 py-1 text-foreground">
       {likedTickers.length}
