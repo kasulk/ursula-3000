4436b0ec6ee44153b5141b809c17ea48f288b098
kasi <danielkaser@mailbox.org>
2024-08-05 15:59:32 +0200

    add placeholders for missing dashboard-, features- and contact pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import { GlowText, BackButton } from "@/components/ui";
+
+export default function ContactPage() {
+  return (
+    <div className="flex flex-col items-center px-4 sm:px-12">
+      <h2 className="mb-12 text-xl">
+        This is going to be the Help & Feedback-Page
+      </h2>
+      <p className="text-lg">
+        <GlowText variant="orange pink">Coming soon!</GlowText>
+      </p>
+      <BackButton className="mt-12 rounded-xl bg-gradient-to-tr from-pink-500 to-orange-500 p-4 py-2 text-sm font-bold uppercase text-white shadow-lg">
+        I don&apos;t believe you!
+      </BackButton>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,22 @@
-import { GlowText, BackButton } from "@/components/ui";
+import Link from "next/link";
+import { GlowText } from "@/components/ui";
+import { Button } from "@nextui-org/react";
 
 export default function DashboardPage() {
   return (
     <div className="flex flex-col items-center px-4 sm:px-12">
-      <h2 className="mb-12 text-xl">This is the Dashboard-Page</h2>
+      <h2 className="mb-12 text-xl">This is going to be the Dashboard-Page</h2>
       <p className="text-lg">
         <GlowText variant="orange pink">Coming soon!</GlowText>
       </p>
-      <BackButton className="mt-12 text-xl">Go Back</BackButton>
+      <Link href="/" className="mt-12">
+        <Button
+          radius="md"
+          className="bg-gradient-to-tr from-pink-500 to-orange-500 font-bold uppercase text-white shadow-lg"
+        >
+          Awesome!
+        </Button>
+      </Link>
     </div>
   );
 }
